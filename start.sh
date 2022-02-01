7c943ee552b84033ddc826e5d2cabada1aad623b
codelrond78 <90870337+codelrond78@users.noreply.github.com>
2022-02-01 12:42:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,4 +30,5 @@ if test -f "$ICARUSINI"; then
     $ICARUSINI > /app/icarus.log 2>&1 &
 fi
 
+echo alias gitlog=\"git log --graph --oneline --all\" >> ~/.bashrc
 code-server --auth password --bind-addr 0.0.0.0:8080 /app
